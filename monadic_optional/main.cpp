b5c1efdd99c05f2d2a25e2339613b1850a44d608
Vladislav Antonov <vladislav.antonov@gmail.com>
2024-12-10 18:40:46 +0200

    Add monadic optional example

---
+++ monadic_optional/main.cpp
@@ -0,0 +1,30 @@
+//https://godbolt.org/z/94GrMfxxn
+
+#include <iostream>
+#include <optional>
+#include <string>
+#include <vector>
+
+std::optional<int> getInt(std::string arg) {
+  try {
+    return {std::stoi(arg)};
+  } catch (...) {
+    return {};
+  }
+}
+
+int main() {
+  std::cout << '\n';
+
+  std::vector<std::optional<std::string>> strings = {"66", "foo", "-5"};
+
+  for (auto s : strings) {
+    auto res = s.and_then(getInt)
+                   .transform([](int n) { return n + 100; })
+                   .transform([](int n) { return std::to_string(n); })
+                   .or_else([] { return std::optional{std::string("Error")}; });
+    std::cout << *res << ' ';
+  }
+
+  std::cout << '\n';
+}
