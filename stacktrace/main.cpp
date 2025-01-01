3764527628ded136a87696e7f7101c03f7d67dcb
Vladislav Antonov <vladislav.antonov@gmail.com>
2025-01-01 11:29:59 +0200

    Add stacktrace example

---
+++ stacktrace/main.cpp
@@ -0,0 +1,17 @@
+// https://godbolt.org/z/9z1jvheEM
+// -std=c++23 -lstdc++_libbacktrace
+// https://gcc.godbolt.org/z/vhxTxv45d
+#include <iostream>
+#include <stacktrace>
+
+int nested_func(int c) {
+  std::cout << std::stacktrace::current() << '\n';
+  return c + 1;
+}
+
+int func(int b) { return nested_func(b + 1); }
+
+int main() {
+  std::cout << func(777);
+  return 0;
+}
